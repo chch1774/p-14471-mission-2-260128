dc056d47ddc632e01a5b73f21e980f352e2c3a9a
chch1774 <cogus1774@gmail.com>
2026-02-08 20:37:16 +0900

    4단계 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,30 @@
-import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int id = 1;
 
-        System.out.println("==명언 앱==");
+        System.out.println("== 명언 앱 ==");
 
-        while (true) {
+        while(true) {
             System.out.print("명령) ");
-            String command = sc.nextLine();
+            String cmd = sc.nextLine();
 
-            if (command.equals("등록")) {
-                System.out.print("명언 : ");
-                sc.nextLine();
-                System.out.print("작가 : ");
-                sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
-            } else if (command.equals("종료")) {
+            if(cmd.equals("종료")) {
                 break;
             }
+
+            if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSaying = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+
+                System.out.println(id + "번 명언이 등록되었습니다.");
+                id++;
+            }
+
         }
     }
 }
