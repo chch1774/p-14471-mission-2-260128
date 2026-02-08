3bb0da21dfae503f4dcc8c11b14fe92f844f5509
chch1774 <cogus1774@gmail.com>
2026-02-08 20:36:35 +0900

    3단계 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,7 @@ public class Main {
                 sc.nextLine();
                 System.out.print("작가 : ");
                 sc.nextLine();
+                System.out.println("1번 명언이 등록되었습니다.");
             } else if (command.equals("종료")) {
                 break;
             }
