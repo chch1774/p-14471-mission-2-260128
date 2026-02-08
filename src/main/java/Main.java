5d80a14ec0c43317e4c958434b7689acd71527d9
chch1774 <cogus1774@gmail.com>
2026-02-08 20:34:26 +0900

    2단계 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 명언 앱 ==");
-        System.out.println("명령) ");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("==명언 앱==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                sc.nextLine();
+                System.out.print("작가 : ");
+                sc.nextLine();
+            } else if (command.equals("종료")) {
+                break;
+            }
+        }
     }
 }
